ba7e111e04dbb0f64edea91bfaac928965b45fb9
YaOn999 <lumous999@gmail.com>
2022-12-10 01:54:22 +0300

    add HW_03

---
+++ HW_03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задаёт массив из 8 
+// элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+// int[] array = new int[8];
+// for (int i = 0; i < 8; i++)
+// {
+//     array[i] = new Random().Next(100);  
+// }
+
+// Console.WriteLine($"[{String.Join(",", array)}]");
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int size = EnterData("Введите размер массива: ");
+
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(100);  
+}
+
+Console.WriteLine($"[{String.Join(",", array)}]");
