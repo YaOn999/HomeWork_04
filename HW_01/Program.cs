3eb463ce3e13703e7ff9050a13486ea63406e569
YaOn999 <lumous999@gmail.com>
2022-12-10 01:49:27 +0300

    add HW_01

---
+++ HW_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+double ExpNumber(int a, int b)
+{
+    double result = Math.Pow(a, b);
+    return result;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int a = EnterData("Введите первое число: ");
+int b = EnterData("Введите второе число: ");
+double exponentiation = ExpNumber(a, b);
+Console.WriteLine($"если число {a} возвести в степень {b} получим {exponentiation}");
