9b35e9d086b5929a6124986796ce358a1d1347ee
YaOn999 <lumous999@gmail.com>
2022-12-10 01:52:36 +0300

    add HW_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNumber(int number)
+{
+    int res = 0;
+    for (int i = 0; i < number; number = number / 10)
+    {
+        res = res + number%10;
+    }
+    return res;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = EnterData("Введите число: ");
+int rest = SumNumber(number);
+Console.WriteLine("сумма цифр в введенном числе " + rest);
